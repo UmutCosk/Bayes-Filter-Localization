86c490a292474703d33b77a1ed9b112d902c8f22
CremeFresh <umut91c@gmail.com>
2019-09-12 20:23:43 +0200

    Added pictures, removed first version

--- test.py
+++
@@ -1,8 +0,0 @@
-import numpy as geek
-
-array = geek.arange(12).reshape(3, 4)
-print("Original array : \n", array)
-
-
-# Rolling array; Shifting five places with 0th axis
-print("\nRolling with 5 shift with 0 axis : \n", geek.roll(array, -1, axis=0))
